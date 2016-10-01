7dd621f4e3015b30c89575f2f903b939f23f565d
podhmo <ababjam61+github@gmail.com>
2016-10-02 08:30:32 +0900

    add test

---
+++ magicalimport/tests/test_it.py
@@ -0,0 +1,38 @@
+import unittest
+import os.path
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+
+class Tests(unittest.TestCase):
+    def _callFUT(self, *args, **kwargs):
+        from magicalimport import import_from_physical_path
+        return import_from_physical_path(*args, **kwargs)
+
+    def test_impot_from_physical_path(self):
+        path = os.path.join(here, "../../examples/a/b/c/foo.py")
+        foo = self._callFUT(path, as_="foo2")
+        self.assertEqual(foo.name, "foo")
+
+        from foo2 import name
+        self.assertEqual(name, "foo")
+
+    def test_expose_all_members(self):
+        from magicalimport import expose_all_members
+        path = os.path.join(here, "../../examples/a/b/c/foo.py")
+        foo = self._callFUT(path, as_="foo2")
+
+        expose_all_members(foo)
+
+        self.assertEqual(name, "foo")  # NOQA
+        with self.assertRaises(NameError):
+            self.assertEqual(_age, "*secret*")  # NOQA
+
+    def test_expose_members(self):
+        from magicalimport import expose_members
+        path = os.path.join(here, "../../examples/a/b/c/foo.py")
+        foo = self._callFUT(path, as_="foo2")
+
+        expose_members(foo, ["_age"])
+
+        self.assertEqual(_age, "*secret*")  # NOQA
