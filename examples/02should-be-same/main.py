2d48f9a51df303a94dd11c3ea5e0714bde842343
podhmo <ababjam61+github@gmail.com>
2019-12-15 22:23:03 +0900

    chore: update examples

--- examples/02should-be-same/main.py
+++ examples/02should-be-same/main.py
@@ -1,8 +1,8 @@
 import sys
 from magicalimport import import_module
 
-commands = import_module("./commands.py", here=__file__)
-di = import_module("./di.py", here=__file__)
+commands = import_module("./app/commands.py", here=__file__)
+di = import_module("./app/di.py", here=__file__)
 
 print(f"@! from {__name__!r} module {di.component.__module__!r}", file=sys.stderr)
 print(
