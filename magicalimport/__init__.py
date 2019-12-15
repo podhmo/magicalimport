6a59ada226be145556cead3b607a4f97db446e62
podhmo <ababjam61+github@gmail.com>
2019-12-15 20:38:47 +0900

    feat: support relative import (same directories module only)

--- magicalimport/__init__.py
+++ magicalimport/__init__.py
@@ -20,22 +20,42 @@ def expose_members(module, members, globals_=None, _depth=1):
 
 
 def _module_id_from_path(path):
+    # print("	->", path, file=sys.stderr)
     path = os.path.normpath(path)
 
     dirname = os.path.dirname(path)
     basename = os.path.basename(path)
 
     module_id = "{}.{}".format(dirname.replace("/", "_"), basename.rsplit(".py", 1)[0])
+    # print("	<-", module_id, file=sys.stderr)
     return module_id
 
 
 def import_from_physical_path(path, as_=None, here=None):
     if here is not None:
         here = here if os.path.isdir(here) else os.path.dirname(here)
-        here = os.path.normpath(os.path.abspath(here))
+        here = os.path.abspath(here)
         path = os.path.join(here, path)
 
     module_id = as_ or _module_id_from_path(path)
+
+    if "." in module_id and as_ is None:
+        parent_module_id = module_id.rsplit(".")[0]
+        if parent_module_id not in sys.modules:
+            init_py = os.path.join(os.path.dirname(path), "__init__.py")
+            if os.path.exists(init_py):
+                _create_module(parent_module_id, init_py)
+            else:
+                # TODO: don't have to create __init__.py
+                with open(init_py, "w"):
+                    pass
+                _create_module(parent_module_id, init_py)
+
+                # # xxx: using fake module as parent module
+                # import magicalimport._fake as fake_module
+
+                # parent_module = _create_module(parent_module_id, fake_module.__file__)
+                # parent_module.__file__ = init_py
 
     if module_id in sys.modules:
         return sys.modules[module_id]
